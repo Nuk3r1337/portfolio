4b28645ee12c4ed1eef5f512c145bbfa1a594610
Nuk3r1337 <John2666@edu.sde.dk>
2019-07-31 14:42:27 +0200

    - Added Cards for projects
    - Added Database connection

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *  Config fil til databasen.
+ */
+
+return [
+
+    "HOSTNAME" => "localhost",
+    "USERNAME" => "johnny",
+    "PASSWORD" => "qawsed123",
+    "DATABASE" => "portfolio",
+    "CHARSET" => "utf8mb4"
+
+];
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+require "classes/database.php";
+
+$database = new Database();
+
+//var_dump($database->query("SELECT * FROM projects WHERE id = ?", [2])->fetchAssoc());
+
+
+?>
 <!DOCTYPE html>
 <html lang="en-uk">
     <head>
@@ -8,21 +18,49 @@
         <meta name="author" content="Johnny K. Pedersen">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <!-- Favicon -->
+        <!--<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>-->
+
         <!-- Bootstrap css -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
         <!-- My css -->
         <link rel="stylesheet" type="text/css" href="resources/stylesheet.css">
 
-        <title>Portfolio</title>
+        <title>Portfolio - Johnny K. Pedersen</title>
     </head>
         <body>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
-            <a class="navbar-brand" href="#" id="textcolor">Portfolio</a>
-
-
+            <a class="navbar-brand" href="/" id="textcolor"> <img src="resources/assets/folder-logo-100px.png" width="35"> Portfolio</a>
         </nav>
+
+        <div class="container" id="topmargin">
+            <div class="card-deck" >
+                <div class="card">
+                    <img class="card-img-top" src="resources/assets/placeholderImage.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="resources/assets/placeholderImage.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="resources/assets/placeholderImage.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
 
         <!-- Optional JavaScript (Bootstrap) -->
